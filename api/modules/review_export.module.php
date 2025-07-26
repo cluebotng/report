db27af715916fa8edda593e9bca2b7c940284e12
Damian Zaremba <damian@damianzaremba.co.uk>
2025-07-26 14:51:58 +0000

    api - review.export - support in progress reports

--- api/modules/review_export.module.php
+++ api/modules/review_export.module.php
@@ -11,7 +11,20 @@ class ApiModuleReviewExport extends ApiModule
     public function content()
     {
         global $mysql;
-        $result = mysqli_query($mysql, 'SELECT `new_id` FROM `reports` JOIN `vandalism` ON `revertid` = `id` WHERE `status` = 2');
+        $status_ids = (isset($_GET['include_in_progress'])) ? array(2, 5, 6) : array(2);
+
+        $status_ids_sql = '';
+        foreach ($status_ids as $status_id) {
+            if (strlen($status_ids_sql) > 0) {
+                $status_ids_sql .= ', ';
+            }
+            $status_ids_sql .= '"' . mysqli_real_escape_string($mysql, $status_id) . '"';
+        }
+
+        $result = mysqli_query(
+            $mysql,
+            'SELECT `new_id` FROM `reports` JOIN `vandalism` ON `revertid` = `id` WHERE `status` IN (' . $status_ids_sql . ')'
+        );
 
         $ids = array();
         while ($row = mysqli_fetch_assoc($result)) {
