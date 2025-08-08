6cfe8d7e7c9cedf141b31e9540332efe73298510
Damian Zaremba <damian@damianzaremba.co.uk>
2025-08-08 15:03:34 +0000

    web-settings - remove static from dynamic variables

--- web-settings.php
+++ web-settings.php
@@ -2,10 +2,10 @@
 
     static $cb_mysql_host = 'tools-db';
     static $cb_mysql_port = 3306;
-    static $cb_mysql_user = getenv('TOOL_TOOLSDB_USER');
-    static $cb_mysql_pass = getenv('TOOL_TOOLSDB_PASSWORD');
-    static $cb_mysql_schema = getenv('TOOL_TOOLSDB_SCHEMA');
+    $cb_mysql_user = getenv('TOOL_TOOLSDB_USER');
+    $cb_mysql_pass = getenv('TOOL_TOOLSDB_PASSWORD');
+    $cb_mysql_schema = getenv('TOOL_TOOLSDB_SCHEMA');
 
-    static $oauth_consumer_key = getenv('OAUTH_KEY');
-    static $oauth_consumer_secret = getenv('OAUTH_SECRET');
+    $oauth_consumer_key = getenv('OAUTH_KEY');
+    $oauth_consumer_secret = getenv('OAUTH_SECRET');
 
