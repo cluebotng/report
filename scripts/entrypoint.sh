a41377be2ea3355c0e43bea0ad2a364dbb29174a
Damian Zaremba <damian@damianzaremba.co.uk>
2025-08-10 16:49:44 +0000

    Store sessions in redis
    This should prevent users being logged out everytime the pods get
    restarted e.g. we do a deploy

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Store sessions in REDIS
+cat > /layers/heroku_php/platform/etc/php/conf.d/sessions.ini <<EOF
+session.save_handler = redis
+session.save_path = "tcp://redis:6379?auth[]=${REDIS_PASSWORD}&database=1"
+
+redis.session.locking_enabled = 1
+redis.session.compression = zstd
+EOF
+
+# Launch apache/php-fpm
+exec launcher heroku-php-apache2
