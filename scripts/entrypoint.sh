d208bda3231f3c6b455e09b73ae6a744ac1aa6cc
Damian Zaremba <damian@damianzaremba.co.uk>
2025-08-11 12:35:42 +0000

    redis - use db 2

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 # Store sessions in REDIS
 cat > /layers/heroku_php/platform/etc/php/conf.d/sessions.ini <<EOF
 session.save_handler = redis
-session.save_path = "tcp://redis:6379?auth[]=${REDIS_PASSWORD}&database=1"
+session.save_path = "tcp://redis:6379?auth[]=${REDIS_PASSWORD}&database=2"
 
 redis.session.locking_enabled = 1
 redis.session.compression = zstd
