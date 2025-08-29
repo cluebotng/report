3b193ec577abf7572cb7c001140ce90b0e1688aa
Damian Zaremba <damian@damianzaremba.co.uk>
2025-08-29 14:05:20 +0000

    api - metrics - add basic bot metrics
    These should really be exported via the bot or scraped separately, for
    now collect them here.

--- api/modules/metrics.module.php
+++ api/modules/metrics.module.php
@@ -20,6 +20,7 @@ class Metrics extends ApiModule
 
         $output = "";
 
+        // Reports
         $output .= "# HELP cbng_report_number_of_reports_by_status Number of reports in by status\n";
         $output .= "# TYPE cbng_report_number_of_reports_by_status gauge\n";
 
@@ -27,6 +28,22 @@ class Metrics extends ApiModule
         while ($row = mysqli_fetch_assoc($report_count_by_status)) {
             $output .= "cbng_report_number_of_reports_by_status{status=\"" . $statuses[$row["status"]] . "\"} " . $row["count"] . "\n";
         }
+
+        // Vandalism
+        $output .= "# HELP cbng_bot_number_of_vandalised_edits Number of vandalised edits detected\n";
+        $output .= "# TYPE cbng_bot_number_of_vandalised_edits gauge\n";
+
+        $vandalism_count = mysqli_query($mysql, "SELECT COUNT(*) as `count`, `reverted` FROM `vandalism` GROUP BY `reverted`");
+        while ($row = mysqli_fetch_assoc($vandalism_count)) {
+            $output .= "cbng_bot_number_of_vandalised_edits{reverted=\"" . $row["reverted"] . "\"} " . $row["count"] . "\n";
+        }
+
+        // Beaten
+        $output .= "# HELP cbng_bot_number_of_reverts_beaten Number of attempted reverts that where beaten\n";
+        $output .= "# TYPE cbng_bot_number_of_reverts_beaten gauge\n";
+
+        $row = mysqli_fetch_assoc(mysqli_query($mysql, "SELECT COUNT(*) as `count` FROM `beaten`"));
+        $output .= "cbng_bot_number_of_reverts_beaten " . $row["count"] . "\n";
 
         return $output;
     }
