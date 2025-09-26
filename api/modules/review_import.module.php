968c83654a2713448dd2cf70ed8d9de871d91ce2
Damian Zaremba <damian@damianzaremba.co.uk>
2025-09-26 20:50:40 +0200

    review_import - fix status exclusion bug
    status 5 is 'Queued to be reviewed' which is the first status that
    review picks up, with it excluded the report never transitions out of
    this state.
    
    Generate the ids using the helper function so the code is easier to
    reason about.
    
    Change the output to include processed as well as updated ids, to aid
    debugging.

--- api/modules/review_import.module.php
+++ api/modules/review_import.module.php
@@ -25,7 +25,6 @@ class ApiModuleReviewImport extends ApiModule
         );
 
         $expected_statuses = array();
-
         $context  = stream_context_create(array('http' => array('user_agent' => 'ClueBot NG Report Interface')));
 
         // We have edits split up e.g. "Legacy Report Interface Import" & "Report Interface Import"
@@ -35,12 +34,13 @@ class ApiModuleReviewImport extends ApiModule
             return json_encode(array("error" => "Failed to retrieve edit groups"), JSON_PRETTY_PRINT);
         }
         $edit_groups = json_decode($raw, true);
+        $processed_groups = array();
         foreach ($edit_groups as $edit_group) {
             if ($edit_group["type"] == "Reported False Positives") {
                 $raw = @file_get_contents('https://cluebotng-review.toolforge.org/api/v1/edit-groups/' . $edit_group["id"] . '/dump-report-status/', false, $context);
                 if ($raw != null) {
-                    $edit_statuses = json_decode($raw);
-                    foreach ($edit_statuses as $diff_id => $review_status_id) {
+                    $processed_groups[] = $edit_group["id"];
+                    foreach (json_decode($raw) as $diff_id => $review_status_id) {
                         if ($report_status = $review_to_report_statuses[(int)$review_status_id]) {
                             $expected_statuses[(int)$diff_id] = $report_status;
                         }
@@ -57,27 +57,44 @@ class ApiModuleReviewImport extends ApiModule
             $diff_ids_sql .= '"' . mysqli_real_escape_string($mysql, (int)$diff_id) . '"';
         }
 
+        $exclude_status_ids = [
+            statusNameToId('Reported'),
+            statusNameToId('Invalid'),
+            statusNameToId('Bug'),
+            statusNameToId('Resolved'),
+            statusNameToId('Edit Data Has Been Removed'),
+        ];
+
         $updated_ids = array();
         if (strlen($diff_ids_sql) > 0) {
             // Load the edits we might want to update
             $query = 'SELECT `revertid`, `new_id`, `status` FROM `reports`';
             $query .= ' INNER JOIN `vandalism` ON (`vandalism`.`id` = `reports`.`revertid`)';
-            $query .= ' WHERE `status` NOT IN (0, 1, 3, 4, 5)';
+            $query .= ' WHERE `status` NOT IN (' . join(", ", $exclude_status_ids) . ')';
             $query .= ' AND `new_id` IN (' . $diff_ids_sql . ')';
 
             // Update them if required
             $results = mysqli_query($mysql, $query);
             while ($row = mysqli_fetch_assoc($results)) {
                 $expected_status = $expected_statuses[(int)$row['new_id']];
+
                 if ($expected_status && (int)$row['status'] !== $expected_status) {
-                    array_push($updated_ids, (int)$row['new_id']);
+                    $updated_ids[] = (int)$row['new_id'];
                     updateStatus($row['revertid'], $expected_status, 'Review Interface', -3);
                 }
             }
             mysqli_free_result($results);
         }
 
-        return json_encode($updated_ids, JSON_PRETTY_PRINT);
+        return json_encode([
+            "groups" => [
+                "processed" => $processed_groups
+            ],
+            "edits" => [
+                "processed" => $expected_statuses,
+                "updated" => $updated_ids
+            ]
+        ], JSON_PRETTY_PRINT);
     }
 }
 
