aa49be03c07ceaa6b6f45a8e288045b3d18d74ce
methecooldude <rsmithy@cluenet.org>
2025-09-28 15:01:42 +0100

    Update reports_list.module.php

--- api/modules/reports_list.module.php
+++ api/modules/reports_list.module.php
@@ -12,11 +12,9 @@ class ApiModuleReportsList extends ApiModule
     {
         global $mysql;
 
-        // Build the base query with placeholders
         $query = "SELECT * FROM `reports` JOIN `vandalism` ON `revertid` = `id`";
         $params = [];
         $types = '';
-        
         if (isset($_REQUEST['status']) && (!empty($_REQUEST['status']) || $_REQUEST['status'] == '0')) {
             if (isValidStatusId($_REQUEST['status'])) {
                 $query .= " WHERE `status` = ?";
@@ -40,8 +38,7 @@ class ApiModuleReportsList extends ApiModule
 
         $data = array();
         $stmt = mysqli_prepare($mysql, $query);
-        
-        // Bind parameters if any
+
         if (!empty($params)) {
             $bindParams = array_merge([$stmt, $types], $params);
             call_user_func_array('mysqli_stmt_bind_param', $bindParams);
@@ -49,7 +46,6 @@ class ApiModuleReportsList extends ApiModule
         
         mysqli_stmt_execute($stmt);
         $report_results = mysqli_stmt_get_result($stmt);
-        
         while ($report_row = mysqli_fetch_assoc($report_results)) {
             $data['report-' . $report_row['revertid']] = array(
                 "revertid" => (int)$report_row['revertid'],
@@ -61,14 +57,12 @@ class ApiModuleReportsList extends ApiModule
                 "comments" => array(),
             );
 
-            // Prepare statement for comments query
             $comment_query = "SELECT * FROM `comments` WHERE `revertid` = ?";
             $comment_stmt = mysqli_prepare($mysql, $comment_query);
             $revertId = $report_row['id'];
             mysqli_stmt_bind_param($comment_stmt, 'i', $revertId);
             mysqli_stmt_execute($comment_stmt);
             $comments_result = mysqli_stmt_get_result($comment_stmt);
-            
             while ($comment_row = mysqli_fetch_assoc($comments_result)) {
                 $data['report-' . $report_row['revertid']]['comments'][] = array(
                     "timestamp" => strtotime($comment_row['timestamp']),
