275e96294f6b2ed0c889d051077014f0eff52ef0
methecooldude <rsmithy@cluenet.org>
2025-09-28 14:58:40 +0100

    Convert to prepared statements

--- api/modules/reports_get.module.php
+++ api/modules/reports_get.module.php
@@ -11,18 +11,25 @@ class ApiModuleReportsGet extends ApiModule
     public function content()
     {
         global $mysql;
-
-        $query = "SELECT * FROM `reports`";
-        if (!empty($_REQUEST['id'])) {
-            $query .= " WHERE `revertid` = '" . mysqli_real_escape_string($mysql, $_REQUEST['id']) . "'";
-        } else {
+        if (empty($_REQUEST['id'])) {
             return json_encode(array(
                 "error" => "argument_error",
                 "error_message" => "Specified id was in an invalid format",
             ));
         }
+        $query = "SELECT * FROM `reports` WHERE `revertid` = ?";
+        $stmt = mysqli_prepare($mysql, $query);
+        if (!$stmt) {
+            return json_encode(array(
+                "error" => "database_error",
+                "error_message" => "Failed to prepare statement",
+            ));
+        }
+        mysqli_stmt_bind_param($stmt, 's', $_REQUEST['id']);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        mysqli_stmt_close($stmt);
 
-        $result = mysqli_query($mysql, $query);
         if (mysqli_num_rows($result) !== 1) {
             return json_encode(array(
                 "error" => "argument_error",
@@ -41,9 +48,17 @@ class ApiModuleReportsGet extends ApiModule
         );
         mysqli_free_result($result);
 
-        $idEscaped = mysqli_real_escape_string($mysql, $report_row['revertid']);
-        $comment_query = "SELECT * FROM `comments` WHERE `revertid` = '" . $idEscaped . "'";
-        $comment_result = mysqli_query($mysql, $comment_query);
+        $comment_query = "SELECT * FROM `comments` WHERE `revertid` = ?";
+        $comment_stmt = mysqli_prepare($mysql, $comment_query);
+        if (!$stmt) {
+            return json_encode(array(
+                "error" => "database_error",
+                "error_message" => "Failed to prepare statement",
+            ));
+        }
+        mysqli_stmt_bind_param($comment_stmt, 's', $report_row['revertid']);
+        mysqli_stmt_execute($comment_stmt);
+        $comment_result = mysqli_stmt_get_result($comment_stmt);
         while ($comment_row = mysqli_fetch_assoc($comment_result)) {
             $data['comments'][] = array(
                 "timestamp" => strtotime($comment_row['timestamp']),
@@ -51,7 +66,7 @@ class ApiModuleReportsGet extends ApiModule
                 "comment" => $comment_row['comment'],
             );
         }
-        mysqli_free_result($comment_result);
+        mysqli_stmt_close($comment_stmt);
 
         return json_encode($data, JSON_PRETTY_PRINT);
     }
--- api/modules/reports_list.module.php
+++ api/modules/reports_list.module.php
@@ -12,10 +12,16 @@ class ApiModuleReportsList extends ApiModule
     {
         global $mysql;
 
+        // Build the base query with placeholders
         $query = "SELECT * FROM `reports` JOIN `vandalism` ON `revertid` = `id`";
+        $params = [];
+        $types = '';
+        
         if (isset($_REQUEST['status']) && (!empty($_REQUEST['status']) || $_REQUEST['status'] == '0')) {
             if (isValidStatusId($_REQUEST['status'])) {
-                $query .= " WHERE `status` = '" . mysqli_real_escape_string($mysql, $_REQUEST['status']) . "'";
+                $query .= " WHERE `status` = ?";
+                $params[] = &$_REQUEST['status'];
+                $types .= 'i';
             } else {
                 return json_encode(array(
                     "error" => "argument_error",
@@ -26,11 +32,24 @@ class ApiModuleReportsList extends ApiModule
 
         $query .= " ORDER BY " . ((isset($_REQUEST['random'])) ? "RAND()" : "id DESC");
         if (!empty($_REQUEST['limit'])) {
-            $query .= " LIMIT 0, " . (int)$_REQUEST['limit'];
+            $limit = (int)$_REQUEST['limit'];
+            $query .= " LIMIT 0, ?";
+            $params[] = &$limit;
+            $types .= 'i';
         }
 
         $data = array();
-        $report_results = mysqli_query($mysql, $query);
+        $stmt = mysqli_prepare($mysql, $query);
+        
+        // Bind parameters if any
+        if (!empty($params)) {
+            $bindParams = array_merge([$stmt, $types], $params);
+            call_user_func_array('mysqli_stmt_bind_param', $bindParams);
+        }
+        
+        mysqli_stmt_execute($stmt);
+        $report_results = mysqli_stmt_get_result($stmt);
+        
         while ($report_row = mysqli_fetch_assoc($report_results)) {
             $data['report-' . $report_row['revertid']] = array(
                 "revertid" => (int)$report_row['revertid'],
@@ -42,9 +61,14 @@ class ApiModuleReportsList extends ApiModule
                 "comments" => array(),
             );
 
-            $idEscaped = mysqli_real_escape_string($mysql, $report_row['id']);
-            $comment_query = "SELECT * FROM `comments` WHERE `revertid` = '" . $idEscaped . "'";
-            $comments_result = mysqli_query($mysql, $comment_query);
+            // Prepare statement for comments query
+            $comment_query = "SELECT * FROM `comments` WHERE `revertid` = ?";
+            $comment_stmt = mysqli_prepare($mysql, $comment_query);
+            $revertId = $report_row['id'];
+            mysqli_stmt_bind_param($comment_stmt, 'i', $revertId);
+            mysqli_stmt_execute($comment_stmt);
+            $comments_result = mysqli_stmt_get_result($comment_stmt);
+            
             while ($comment_row = mysqli_fetch_assoc($comments_result)) {
                 $data['report-' . $report_row['revertid']]['comments'][] = array(
                     "timestamp" => strtotime($comment_row['timestamp']),
@@ -52,9 +76,9 @@ class ApiModuleReportsList extends ApiModule
                     "comment" => $comment_row['comment'],
                 );
             }
-            mysqli_free_result($comments_result);
+            mysqli_stmt_close($comment_stmt);
         }
-        mysqli_free_result($report_results);
+        mysqli_stmt_close($stmt);
 
         return json_encode($data, JSON_PRETTY_PRINT);
     }
